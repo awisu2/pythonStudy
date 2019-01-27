a52506ac6771d4df355fde5aa7cfd6b98ee4c5c3
awisu2 <awisu2@gmail.com>
2019-01-27 15:35:29 +0900

    [add] @propertyによるsetterとgetterの追記

--- class/01_simple.py
+++ class/01_simple.py
@@ -1,23 +1,30 @@
 class Base(object):
+  # 普通に記述するとパブリックアクセス可能なプロパティ
+  pubname = ''
+
   # "__"をつけるとプライベート
   __name = ''
-  pubname = ''
 
   @classmethod
   def create(cls, name):
     return cls(name)
 
   def __init__(self, name):
-    print(f'__name: {self.__name}, pubname: {self.pubname}')
+    print(f'check property already set: {self.__name}, {self.pubname}')
 
     self.__name = name
     # テストのため、pub_をつけてセット
     self.pubname = 'pub_' + name
 
-    print(f'__name: {self.__name}, pubname: {self.pubname}')
-
+  # いわゆるgetter
+  @property
   def name(self):
     return self.__name
+
+  # いわゆるsetter
+  @name.setter
+  def name(self, name):
+    self.__name = name
 
 class Sub(Base):
   def __init__(self, subname, *args, **kwargs):
@@ -33,14 +40,20 @@ class SubNoInit(Base):
 def main():
   print('-- base --')
   base = Base('base')
-  print(base.name())
+  print(base.name, base.pubname)
+
+  base.name = 'change name base'
+  base.__name = 'change name ggggg' # エラーにはならないが値の変更は起きない
+  base.pubname = 'change pubname base'
+  print(base.name, base.pubname)
 
   print('-- sub --')
   sub = Sub('sub', 'sub-base')
-  print(sub.name(), sub.subname())
+  print(sub.name, sub.subname())
 
   print('-- sub no init --')
-  SubNoInit('sub no init')
+  sub_no_init = SubNoInit('sub no init')
+  print(sub_no_init.name)
 
 if __name__ == "__main__":
   main()
